ca2086af96e9eff6f7bf8fae3d04cfd40f18f76c
Christopher Hipper <hipper.christopher@gmail.com>
2025-02-17 20:34:48 +0100

    render basket and add/remove from basket

--- script.js
+++ script.js
@@ -11,6 +11,12 @@ function renderMainDishes () {
     };
 };
 
+function addToMainBasket(indexMain) {
+    document.getElementById('empty_basket').classList.add('display_none');
+    let basketRef = document.getElementById('basket')
+    basketRef.innerHTML += getMainDishBasketTemplate(indexMain);
+}
+
 function renderSaladDishes () {
     let saladDishesRef = document.getElementById('salad_dish_content')
     for (let indexSalad = 0; indexSalad < salads.length; indexSalad++) {
@@ -18,9 +24,49 @@ function renderSaladDishes () {
     };
 };
 
+function addToSaladBasket(indexSalad) {
+    document.getElementById('empty_basket').classList.add('display_none');
+    let basketRef = document.getElementById('basket')
+    basketRef.innerHTML += getSaladDishBasketTemplate(indexSalad);
+}
+
 function renderDrinks () {
     let drinksRef = document.getElementById('drinks_dish_content')
     for (let indexDrinks = 0; indexDrinks < drinks.length; indexDrinks++) {
         drinksRef.innerHTML += getDrinksTemplate(indexDrinks);
     };
 };
+
+function addToDrinksBasket(indexDrinks) {
+    document.getElementById('empty_basket').classList.add('display_none');
+    let basketRef = document.getElementById('basket')
+    basketRef.innerHTML += getDrinksBasketTemplate(indexDrinks);
+}
+
+function removeFromMainBasket(indexMain) {
+    let removeMainRef = document.getElementById("main" + indexMain);
+    removeMainRef.remove();
+    showPlaceholder()
+}
+
+function removeFromSaladBasket(indexSalad) {
+    let removeMainRef = document.getElementById("salad" + indexSalad);
+    removeMainRef.remove();
+    showPlaceholder()
+}
+
+function removeFromDrinksBasket(indexDrinks) {
+    let removeMainRef = document.getElementById("drinks" + indexDrinks);
+    removeMainRef.remove();
+    showPlaceholder()
+}
+
+function showPlaceholder() {
+    let placeholderRef = document.getElementById('basket')
+    if (placeholderRef != null && placeholderRef.innerHTML.trim() === '') {
+        document.getElementById('empty_basket').classList.remove('display_none');
+    }else
+        return;
+}
+
+
--- scripts/template.js
+++ scripts/template.js
@@ -5,7 +5,20 @@ function getMainDishesTemplate(indexMain) {
                     <span>${mainDishes[indexMain].description}</span>
                     <p>${mainDishes[indexMain].price.toFixed(2).replace(".",",")}€</p>
                 </div>
-                <button>+</button>
+                <button onclick="addToMainBasket(${indexMain})">+</button>
+            </div>`
+}
+
+function getMainDishBasketTemplate(indexMain) {
+    return `<div id=${"main"+ indexMain}>
+                <h3>${mainDishes[indexMain].name}</h3>
+                <div class="basket_content">
+                    <button>-</button>
+                    <span>1x</span>
+                    <button>+</button>
+                    <span>${mainDishes[indexMain].price.toFixed(2).replace(".",",")}€</span>
+                    <img onclick="removeFromMainBasket(${indexMain})" class="trash_img" src="./assets/icon/mulleimer.png" alt="">
+                </div>
             </div>`
 }
 
@@ -16,7 +29,20 @@ function getSaladDishesTemplate(indexSalad) {
                     <span>${salads[indexSalad].description}</span>
                     <p>${salads[indexSalad].price.toFixed(2).replace(".",",")}€</p>
                 </div>
-                <button>+</button>
+                <button onclick="addToSaladBasket(${indexSalad})">+</button>
+            </div>`
+}
+
+function getSaladDishBasketTemplate(indexSalad) {
+    return ` <div id=${"salad"+ indexSalad}>
+                <h3>${salads[indexSalad].name}</h3>
+                <div class="basket_content">
+                    <button>-</button>
+                    <span>1x</span>
+                    <button>+</button>
+                    <span>${salads[indexSalad].price.toFixed(2).replace(".",",")}€</span>
+                    <img onclick="removeFromSaladBasket(${indexSalad})" class="trash_img" src="./assets/icon/mulleimer.png" alt="">
+                </div>
             </div>`
 }
 
@@ -27,8 +53,23 @@ function getDrinksTemplate(indexDrinks) {
                     <span>${drinks[indexDrinks].description}</span>
                     <p>${drinks[indexDrinks].price.toFixed(2).replace(".",",")}€</p>
                 </div>
-                <button>+</button>
+                <button onclick="addToDrinksBasket(${indexDrinks})">+</button>
             </div>`
 }
+
+function getDrinksBasketTemplate(indexDrinks) {
+    return `<div id=${"drinks"+ indexDrinks} >  
+                <h3>${drinks[indexDrinks].name}</h3>
+                <div class="basket_content">
+                    <button>-</button>
+                    <span>1x</span>
+                    <button>+</button>
+                    <span>${drinks[indexDrinks].price.toFixed(2).replace(".",",")}€</span>
+                    <img onclick="removeFromDrinksBasket(${indexDrinks})" class="trash_img" src="./assets/icon/mulleimer.png" alt="">
+                </div>
+            </div>`
+}
+
+
                 
                 
